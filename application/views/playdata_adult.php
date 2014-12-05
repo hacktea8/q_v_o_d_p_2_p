bb1291f41b3bf9265cc661d71c7807cc16dcc58e
jason <1187247901@qq.com>
2014-12-05 11:17:31 +0800

    update

---
+++ application/views/playdata_adult.php
@@ -0,0 +1,15 @@
+<!Doctype html>
+<html xmlns=http://www.w3.org/1999/xhtml>
+<head>
+<meta http-equiv=Content-Type content="text/html;charset=utf-8">
+<title>溫馨提示:尊敬的<?php echo $uinfo['uname'];?>會員，您無權訪問此頁面的內容 </title>
+</head>
+<body>
+ <div class="tipDiv">
+  <h3><?php if( !$uinfo['uid']){echo '抱歉您還未登錄!請登錄后在來~~~~';}else{echo '抱歉您觀看影片所需的點數不夠了';}?></h3>
+  <div class="msgDiv">
+  <?php if( !$uinfo['uid']){echo '<a href="/maindex/login/?goto=/maindex/views/'.$vid.'" target="_top">點我登錄</a>';}else{echo '每部影片消耗8點,如何獲取觀看點數?<a href="" target="_top">點我悄悄告訴你</a>';}?>
+  </div>
+ </div>
+</body>
+</html>
